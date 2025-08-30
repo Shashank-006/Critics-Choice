6f18c2c30d475493087415830adc41c3e268c070
Shashank Shridhar <173191559+Shashank-006@users.noreply.github.com>
2025-08-30 09:11:21 -0400

    Create function to populate TMDB vals

--- server/APIs/fetchInfo.ts
+++ server/APIs/fetchInfo.ts
@@ -12,6 +12,14 @@ interface OMDBResult {
     [key: string]: any
 }
 
+interface TMDBResult {
+    vote_average: number,
+    vote_count: number,
+    poster_path: string,
+    [key: string]: any
+    
+}
+
 export async function fetch250WatchMode(page: number, type: string) {
     const netflix = '203';
     const url = `https://api.watchmode.com/v1/list-titles/?apiKey=${process.env.WATCHMODE_KEY || ""}&source_ids=${netflix}&types=${type}&limit=250&page=${page}`;
@@ -26,6 +34,24 @@ export async function fetchOMDB(imdb_id: string): Promise<OMDBResult> {
     const url = `http://www.omdbapi.com/?apikey=${process.env.OMDB_KEY || ""}&i=${imdb_id}`
 
     const Response = await fetch(url);
+    const res = await Response.json();
+    if(res.Response === "False") {
+        console.log(`imdb_id that failed to load: ${imdb_id}`);
+        throw new Error(res.Error);
+    }
+    return res;
+}
+
+export async function fetchTMDB(tmdb_id: string): Promise<TMDBResult> {
+    const url = `https://api.themoviedb.org/3/movie/${tmdb_id}`;
+    const options = {
+        headers: {
+            accept: "application/json",
+            Authorization: `Bearer ${process.env.TMDB_KEY || ''}`
+        }
+    }
+
+    const Response = await fetch(url, options);
     const res = await Response.json();
     return res;
 }
--- server/db/populateDB.ts
+++ server/db/populateDB.ts
@@ -1,6 +1,7 @@
 import { Client } from "pg";
-import { fetch250WatchMode, fetchOMDB } from "../APIs/fetchInfo";
+import { fetch250WatchMode, fetchOMDB, fetchTMDB } from "../APIs/fetchInfo";
 import dotenv from "dotenv";
+import { count } from "console";
 dotenv.config();
 
 interface Movie {
@@ -85,44 +86,71 @@ export async function populateDB() {
     console.log(`DB populated: ${totalResults} movies added!`);
 }
 
-async function populateOMDB(start: number, end: number) {
-    function updateValue(imdb_id: string, value: any, label: string) {
-        if(value !== "N/A"){
+function updateValue(idLabel: string, client: Client){
+    return (id: string, value: any, label: string) => {
+        if(value !== "N/A" && !Number.isNaN(value)) {
             return client.query(`
                 update movies
                 set ${label}=$1
-                where imdb_id=$2`
-            , [value, imdb_id]);
+                where ${idLabel}=$2`
+            , [value, id]);
         }
     }
+}
+
+async function populateOMDB(start: number, end: number) {
     const client = new Client();
     await client.connect();
+    const updateValueIMDB = updateValue("imdb_id", client);
     const { rows }: { rows: Movie[] } = await client.query("select * from movies where id >= $1 and id <= $2;", [start, end]);
-    const queryPromises = rows.map(async row => {
+    for(const row of rows){
         const details = await fetchOMDB(row.imdb_id);
+
         const imdbVotesInt = details.imdbVotes
                 .split("")
                 .filter(c => c != ',')
                 .join("");
         const runtimeInt = details.Runtime.slice(0, -4);
-        await updateValue(row.imdb_id, details.imdbRating, "imdbRating");
-        await updateValue(row.imdb_id, details.Awards, "awards");
-        await updateValue(row.imdb_id, details.Metascore, "metascore");
-        await updateValue(row.imdb_id, imdbVotesInt, "imdbVotes");
-        await updateValue(row.imdb_id, details.Plot, "plot");
-        await updateValue(row.imdb_id, details.genre, "genre");
-        await updateValue(row.imdb_id, runtimeInt, "runtime");
-    });
-    await Promise.all(queryPromises);
-    
+        await updateValueIMDB(row.imdb_id, Number(details.imdbRating), "imdbRating");
+        await updateValueIMDB(row.imdb_id, details.Awards, "awards");
+        await updateValueIMDB(row.imdb_id, Number(details.Metascore), "metascore");
+        await updateValueIMDB(row.imdb_id, Number(imdbVotesInt), "imdbVotes");
+        await updateValueIMDB(row.imdb_id, details.Plot, "plot");
+        await updateValueIMDB(row.imdb_id, details.Genre, "genre");
+        await updateValueIMDB(row.imdb_id, Number(runtimeInt), "runtime");
+        await updateValueIMDB(row.imdb_id, Number(details.Year), "year");
+    } 
     await client.end();
+}
+
+async function populateTMDB(start: number, end: number) {
+    const client = new Client();
+    await client.connect();
+    const updateValueTMDB = updateValue("tmdb_id", client);
+    const { rows }: { rows: Movie[] } = await client.query("select * from movies where id >= $1 and id <= $2", [start, end]);
+    for(const row of rows){
+        const details = await fetchTMDB(row.tmdb_id);
+        await updateValueTMDB(row.tmdb_id, `https://image.tmdb.org/t/p/w500/${details.poster_path}`, "img");
+    }
+    await client.end();
+}
+
+async function getCount() {
+    const client = new Client();
+    await client.connect();
+    const { rows } = await client.query("select count(*) from movies;");
+    await client.end();
+    return rows[0].count;
 }
 
 async function dropAndPopulate() {
     // await dropDB();
     // await initializeDB();
     // await populateDB();
-    await populateOMDB(1, 10);
+    const count = await getCount();
+    //Usage: Because of rate limiting do groups of 1000, (populateOMDB(1, 1000), populateOMDB(1001, 2000), etc) and change keys for omdb between each run
+    //await populateOMDB(1, count);
+    await populateTMDB(1486, count);
 }
 
 dropAndPopulate();
